dac5e9891f27dc871f1f9ff7a17fa8f1e68b908e
황선하 <tjsgkdia@hwangseonhaui-MacBookPro.local>
2026-08-09 16:25:51 +0900

    fix: 라우팅 오류 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "../src/routes/router";
+import { AuthProvider } from "./shared/lib/authContext";
+import { PageStateProvider } from "./shared/lib/pageState";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <AuthProvider>
+      <PageStateProvider>
+        <RouterProvider router={router} />
+      </PageStateProvider>
+    </AuthProvider>
+  );
 }
 
 export default App;
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,14 +1,64 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Layout } from "../shared/layouts/RootLayout";
+import { Layout, AuthLayout } from "../shared/layouts/RootLayout";
 import Home from "../pages/home/Home";
+import Map from "../pages/map/Map";
+import MyPage from "../pages/mypage/MyPage";
+import SinginPage from "../pages/signin/SinginPage";
+import TermsPage from "../pages/signin/TermsPage";
+import KakaoCallbackPage from "../pages/signin/KakaoCallbackPage";
+import SearchPage from "../pages/search/SearchPage";
+import WineryListPage from "../pages/explore/WineryListPage";
+import WineryDetailPage from "../pages/winery/WineryDetailPage";
+import CourseDetailPage from "../pages/course/CourseDetailPage";
 
 export const router = createBrowserRouter([
   {
-    element: <Layout />, // Layout으로 감싸기
+    element: <Layout />,
     children: [
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/map",
+        element: <Map />,
+      },
+      {
+        path: "/mypage",
+        element: <MyPage />,
+      },
+      {
+        path: "/explore",
+        element: <WineryListPage />,
+      },
+      {
+        path: "/winery/:id",
+        element: <WineryDetailPage />,
+      },
+      {
+        path: "/course/:id",
+        element: <CourseDetailPage />,
+      },
+    ],
+  },
+  {
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "/signin",
+        element: <SinginPage />,
+      },
+      {
+        path: "/signin/terms",
+        element: <TermsPage />,
+      },
+      {
+        path: "/oauth/callback/kakao",
+        element: <KakaoCallbackPage />,
+      },
+      {
+        path: "/search",
+        element: <SearchPage />,
       },
     ],
   },
